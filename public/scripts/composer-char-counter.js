b645ee492f884fb4bf0c7d6cf35cb3de10b6e844
noahthomlison <noahthomlison@gmail.com>
2021-09-09 04:03:21 +0000

    updated things

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,17 @@
+/*
+ * Client-side JS logic goes here
+ * jQuery is already loaded
+ * Reminder: Use (and do all your DOM work in) jQuery's document ready function
+ */
+
+
+$(document).ready(function() {
+  // --- our code goes here ---
+  console.log('properly loaded')
+  $('#tweet-text').addeventlistener("click", () => {
+    console.log('ding')
+  })
+
+
+
+});
