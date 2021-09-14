d2fdb93213c36f26648dacf1720e8353cf34a7a9
noahthomlison <noahthomlison@gmail.com>
2021-09-14 04:12:44 +0000

    added another function which calls the function which creates the markup fand loops through an array of tweets and appends them all to the dom

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,6 +3,32 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
+
+const tweetData = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+]
+
 const createTweetElement = (tweetData) => {
   const htmlTweetMarkup = `
   <article class="oldTweets">
@@ -28,25 +54,15 @@ const createTweetElement = (tweetData) => {
     </footer>
   </article>`
 
+  console.log(htmlTweetMarkup)
   return htmlTweetMarkup
 }
 
-// Test / driver code (temporary). Eventually will get this from the server.
-const tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
-      "handle": "@SirIsaac"
-    },
-  "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-  "created_at": 1461116232227
+const renderTweets = (tweets) => {
+  tweets.forEach(tweet => {
+    tweetToRender = createTweetElement(tweet)
+    $('#tweets-container').append(tweetToRender);
+  });
 }
 
-const $tweet = createTweetElement(tweetData);
-
-// Test / driver code (temporary)
-console.log($tweet); // to see what it looks like
-
-$('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+renderTweets(tweetData);
